b7a9b6e1735b562e0b0648132cea5305107bab31
omccully <oscarmccully@gmail.com>
2023-05-16 23:04:34 -0500

    simplify unit tests and add more for SubnetMaskComponent

--- SubnetCalculator/src/app/subnet-mask/subnet-mask.component.spec.ts
+++ SubnetCalculator/src/app/subnet-mask/subnet-mask.component.spec.ts
@@ -8,6 +8,30 @@ describe('SubnetMaskComponent', () => {
   let component: SubnetMaskComponent;
   let fixture: ComponentFixture<SubnetMaskComponent>;
 
+  function initializeSubnetMaskInput(subnetMask: string) {
+    component.subnetMask = subnetMask;
+    component.ngOnChanges({
+      "subnetMask": new SimpleChange(null, subnetMask, true)
+    })
+    fixture.detectChanges();
+  }
+
+  function escapeRegex(regex: string) {
+    return regex.replace(/[/\-\\^$*+?.()|[\]{}]/g, '\\$&');
+  }
+
+  function expectElementTextMatch(elementSelector: string, regex: string) {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const element = compiled.querySelector(elementSelector);
+    const textContent = element?.textContent;
+    expect(textContent).toMatch(regex)
+  }
+
+  function expectInfoElement(elementSelector: string, expectedLabel: string, expectedValue: string) {
+    const regex = escapeRegex(expectedLabel) + '.+: ' + escapeRegex(expectedValue);
+    expectElementTextMatch(elementSelector, regex);
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
@@ -33,24 +57,19 @@ describe('SubnetMaskComponent', () => {
 
   it('should display slash notation', () => {
     const subnetMask = "255.255.0.0";
-    component.subnetMask = subnetMask;
-    component.ngOnChanges({
-      "subnetMask": new SimpleChange(null, subnetMask, true)
-    })
-    expect(component.oneBitCount).toEqual(16);
+    initializeSubnetMaskInput(subnetMask);
+    expectInfoElement('.sn-info-slash-notation', 'Slash notation', '/16');
+  });
 
-    fixture.detectChanges();
-    
-    const compiled = fixture.nativeElement as HTMLElement;
-    const element = compiled.querySelector('.sn-info-slash-notation');
-    const textContent = element?.textContent;
-    expect(textContent).toMatch('Slash notation:.+\/16')
+  it('should display hosts per subnet', () => {
+    const subnetMask = "255.255.0.0";
+    initializeSubnetMaskInput(subnetMask);
+    expectInfoElement('.sn-info-hosts-per-subnet', 'Hosts per subnet', '65534');
+  });
+
+  it('should display number of subnets', () => {
+    const subnetMask = "255.192.0.0";
+    initializeSubnetMaskInput(subnetMask);
+    expectInfoElement(".sn-info-number-of-subnets", 'Number of subnets', '4');
   });
 });
-
-//@Component({
-//  selector: 'app-help-tool-tip',
-//  template: ''
-//})
-//class MockHelpToolTipComponent {
-//}
